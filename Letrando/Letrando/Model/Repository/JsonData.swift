7c65c2a1571904ab3517702e0e4b37c43be8da0b
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2020-11-23 11:29:02 -0300

    Reading json file and transforming it into the necessary structure

---
+++ Letrando/Letrando/Model/Repository/JsonData.swift
@@ -0,0 +1,49 @@
+//
+//  JsonData.swift
+//  Letrando
+//
+//  Created by Kellyane Nogueira on 23/11/20.
+//
+
+import Foundation
+
+//Load json
+//Create parse method
+
+class JsonData {
+    
+    let name = "words"
+    
+    func readLocalFile() -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
+               let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
+    func parse() -> [Word]{
+        do {
+            let jsonData = readLocalFile()
+            let decodedData = try JSONDecoder().decode([Word].self,
+                                                       from: jsonData)
+            return decodedData
+
+        } catch {
+            return []
+        }
+    }
+    
+    func randomWord() -> Word? {
+        guard let words = parse().randomElement() else {
+            return nil
+        }
+        
+        return words
+    }
+}
