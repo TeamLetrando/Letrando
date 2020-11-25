d069558c5d04e9cd6caccc85f6accfc2312828c4
Lidiane <lidianegomesbarbosa@gmail.com>
2020-11-25 14:51:01 -0300

    create letters in ARModel

--- Letrando/Letrando/Model/Entities/ARModel.swift
+++ Letrando/Letrando/Model/Entities/ARModel.swift
@@ -10,15 +10,27 @@ import SceneKit
 
 class ARModel {
 
-    var word: Word?
+    static func createTextNode(string: String) -> SCNNode {
+        let text = SCNText(string: string, extrusionDepth: 0.3)
+        text.font = UIFont.systemFont(ofSize: 3.0)
+        text.flatness = 0.01
+        text.firstMaterial?.diffuse.contents = UIColor.white
 
-    func transformWordInSCNText() -> [SCNText] {
-        guard let word = word else {
-            return []
-        }
-        let scnTexts = word.breakInLetters().map {
-            SCNText(string: $0.uppercased(), extrusionDepth: 0.5)
-        }
-        return scnTexts
+        let textNode = SCNNode(geometry: text)
+        textNode.castsShadow = true
+
+        let fontSize = Float(0.07)
+        textNode.scale = SCNVector3(fontSize, fontSize, fontSize)
+
+        var minVec = SCNVector3Zero
+        var maxVec = SCNVector3Zero
+        (minVec, maxVec) =  textNode.boundingBox
+        textNode.pivot = SCNMatrix4MakeTranslation(
+            minVec.x + (maxVec.x - minVec.x)/2,
+            minVec.y,
+            minVec.z + (maxVec.z - minVec.z)/2
+        )
+
+        return textNode
     }
 }
--- Letrando/Letrando/Model/Entities/Scene.swift
+++ Letrando/Letrando/Model/Entities/Scene.swift
@@ -50,34 +50,11 @@ struct Scene {
     mutating func addLetterToScene(letter: String, parent: SCNNode, position: SCNVector3 = SCNVector3Zero) {
         guard let scene = self.scene else { return }
 
-            let letterNode = createTextNode(string: letter)
+        let letterNode = ARModel.createTextNode(string: letter)
             letterNode.position = scene.rootNode.convertPosition(position, to: parent)
             parent.addChildNode(letterNode)
             textNode.append(letterNode)
 
     }
 
-    func createTextNode(string: String) -> SCNNode {
-        let text = SCNText(string: string, extrusionDepth: 0.3)
-        text.font = UIFont.systemFont(ofSize: 3.0)
-        text.flatness = 0.01
-        text.firstMaterial?.diffuse.contents = UIColor.white
-
-        let textNode = SCNNode(geometry: text)
-        textNode.castsShadow = true
-
-        let fontSize = Float(0.07)
-        textNode.scale = SCNVector3(fontSize, fontSize, fontSize)
-
-        var minVec = SCNVector3Zero
-        var maxVec = SCNVector3Zero
-        (minVec, maxVec) =  textNode.boundingBox
-        textNode.pivot = SCNMatrix4MakeTranslation(
-            minVec.x + (maxVec.x - minVec.x)/2,
-            minVec.y,
-            minVec.z + (maxVec.z - minVec.z)/2
-        )
-
-        return textNode
-    }
 }
