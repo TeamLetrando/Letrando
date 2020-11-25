7b447934f868c42932f596e7a752252eafadf4dd
Lidiane <lidianegomesbarbosa@gmail.com>
2020-11-25 10:11:10 -0300

    extension UIColor

--- Letrando/Letrando/Utils/Colors/Colors.swift
+++ Letrando/Letrando/Utils/Colors/Colors.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+import UIKit
+extension UIColor {
+    open class var transparentLightBlue: UIColor {
+        return UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 0.50)
+    }
+}
