4249f855f1e781c6e8db0c710ab35212231fca65
Patricia <patysiq@yahoo.com.br>
2020-11-25 08:48:24 -0300

    some things design

--- Letrando/Letrando/Utils/Colors/Colors.swift
+++ Letrando/Letrando/Utils/Colors/Colors.swift
@@ -5,4 +5,13 @@
 //  Created by Ronaldo Gomes on 20/11/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let lightGreenBack = UIColor(red: 208/255, green: 227/255, blue: 219/255, alpha: 1)
+    static let bronwLetters = UIColor(red: 111/255, green: 96/255, blue: 94/255, alpha: 1)
+    static let greenButtons = UIColor(red: 96/255, green: 193/255, blue: 96/255, alpha: 1)
+    static let purpleLetters = UIColor(red: 143/255, green: 97/255, blue: 168/255, alpha: 1)
+    static let white = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let whiteViews = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
+}
