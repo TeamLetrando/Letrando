bd983ae6879dc7483b88fd8a2b95c33dd2df24f3
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-12-01 11:36:20 -0300

    Create result screen with transition between seacrh screen

--- Letrando/Letrando/Controller/SearchResult/SearchResultViewController.swift
+++ Letrando/Letrando/Controller/SearchResult/SearchResultViewController.swift
@@ -9,20 +9,44 @@ import UIKit
 
 class SearchResultViewController: UIViewController {
 
+    @IBOutlet weak var mensageLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var outButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupLayoutOfItems()
     }
 
-    /*
-    // MARK: - Navigation
+    func setupLayoutOfItems() {
+        mensageLabel.textColor = .bronwLetters
+        mensageLabel.font = UIFont(name: "BubblegumSans-Regular", size: 60)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        wordLabel.textColor = .purpleLetters
+        wordLabel.text = wordLabel.text?.uppercased()
+        wordLabel.font = .systemFont(ofSize: 90)
+
+        searchButton.backgroundColor = .greenButtons
+        searchButton.titleLabel?.textColor = .white
+        searchButton.titleLabel?.font = UIFont(name: "BubblegumSans-Regular", size: 40)
+        searchButton.layer.cornerRadius = 10
+
+        outButton.backgroundColor = .bronwLetters
+        outButton.titleLabel?.textColor = .white
+        outButton.titleLabel?.font = UIFont(name: "BubblegumSans-Regular", size: 40)
+        outButton.layer.cornerRadius = 10
     }
-    */
 
+    @IBAction func searchAgain(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Search", bundle: nil)
+        guard let viewC =  storyboard.instantiateViewController(identifier: "search")
+                as? SearchViewController else {fatalError()}
+        viewC.modalPresentationStyle = .fullScreen
+        self.present(viewC, animated: true, completion: nil)
+    }
+
+    @IBAction func exitScrenn(_ sender: UIButton) {
+    }
 }
