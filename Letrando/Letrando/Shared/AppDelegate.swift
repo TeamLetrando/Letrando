06f7be6ea3bc985fe26c3f8a09fbea4166ff9464
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-08-09 11:46:50 -0300

    Fix app delegate error

--- Letrando/Letrando/Shared/AppDelegate.swift
+++ Letrando/Letrando/Shared/AppDelegate.swift
@@ -24,17 +24,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             UserDefaults.standard.set(true, forKey: "checkSound")
         }
 
-        guard ARWorldTrackingConfiguration.isSupported else {
-            fatalError("""
-                ARKit is not available on this device. For apps that require ARKit
-                for core functionality, use the `arkit` key in the key in the
-                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
-                the app from installing. (If the app can't be installed, this error
-                can't be triggered in a production scenario.)
-                In apps where AR is an additive feature, use `isSupported` to
-                determine whether to show UI for launching AR experiences.
-            """) // For details, see https://developer.apple.com/documentation/arkit
-        }
+//        guard ARWorldTrackingConfiguration.isSupported else {
+//            fatalError("""
+//                ARKit is not available on this device. For apps that require ARKit
+//                for core functionality, use the `arkit` key in the key in the
+//                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+//                the app from installing. (If the app can't be installed, this error
+//                can't be triggered in a production scenario.)
+//                In apps where AR is an additive feature, use `isSupported` to
+//                determine whether to show UI for launching AR experiences.
+//            """) // For details, see https://developer.apple.com/documentation/arkit
+//        }
         
         UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) {(granted, _ ) in
