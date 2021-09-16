a1655fb77715cc1a65ae8360735c287c6845019f
lidianegb <lidianegomesbarbosa@gmail.com>
2021-09-16 10:45:09 -0300

    correção github actions

--- Letrando/Letrando/Utils/Extensions/UIFont.swift
+++ Letrando/Letrando/Utils/Extensions/UIFont.swift
@@ -9,15 +9,14 @@ import UIKit
 
 extension UIFont {
     static func set(size: CGFloat, weight: UIFont.Weight, textStyle: UIFont.TextStyle) -> UIFont {
-        var roundedFont: UIFont?
+
         let systemFont = systemFont(ofSize: size, weight: weight)
         
         if let fontDescriptor = systemFont.fontDescriptor.withDesign(.rounded) {
-           roundedFont = UIFont(descriptor: fontDescriptor, size: size)
+            let roundedFont = UIFont(descriptor: fontDescriptor, size: size)
+            return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: roundedFont)
         }
-        guard let roundedFont = roundedFont else {
-            return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: systemFont)
-        }
-        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: roundedFont)
+     
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: systemFont)
     }
 }
