69df77ac39b168a6a98bdc6e08dcfb784a068e2f
lidianegb <lidianegomesbarbosa@gmail.com>
2021-09-17 11:10:14 -0300

    variavel renomeada

--- Letrando/Letrando/Utils/Extensions/UIFont.swift
+++ Letrando/Letrando/Utils/Extensions/UIFont.swift
@@ -10,13 +10,13 @@ import UIKit
 extension UIFont {
     static func set(size: CGFloat, weight: UIFont.Weight, textStyle: UIFont.TextStyle) -> UIFont {
 
-        let systemFont = systemFont(ofSize: size, weight: weight)
+        let customFont = systemFont(ofSize: size, weight: weight)
         
-        if let fontDescriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+        if let fontDescriptor = customFont.fontDescriptor.withDesign(.rounded) {
             let roundedFont = UIFont(descriptor: fontDescriptor, size: size)
             return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: roundedFont)
         }
      
-        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: systemFont)
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: customFont)
     }
 }
