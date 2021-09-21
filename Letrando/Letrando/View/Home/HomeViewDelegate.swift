aae97033dd578d52c4a1e39e8b3b23202f7e159d
lidianegb <lidianegomesbarbosa@gmail.com>
2021-09-21 09:42:36 -0300

    delegates

--- Letrando/Letrando/View/Home/HomeViewDelegate.swift
+++ Letrando/Letrando/View/Home/HomeViewDelegate.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-protocol GameViewDelegate: AnyObject {
+protocol HomeViewDelegate: AnyObject {
     func startGame()
 }
---
+++ Letrando/Letrando/View/Result/ResultViewDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewDelegate.swift
+//  Letrando
+//
+//  Created by Lidiane Gomes Barbosa on 21/09/21.
+//
+
+import Foundation
