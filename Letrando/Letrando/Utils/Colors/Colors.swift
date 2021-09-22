b80819d51aef1a03e8c8357a700f7eddd0bb9f95
MariaKellyane <kellyane.nogueira@popcode.com.br>
2021-09-22 08:42:45 -0300

    Add new settings style letters

--- Letrando/Letrando/Utils/Colors/Colors.swift
+++ Letrando/Letrando/Utils/Colors/Colors.swift
@@ -32,4 +32,18 @@ extension UIColor {
     static let greenActionLetrando = UIColor(red: 96/255, green: 193/255, blue: 96/255, alpha: 1)
     static let greenBackgroundLetrando = UIColor(red: 197/255, green: 237/255, blue: 223/255, alpha: 1)
     
+    // MARK: - Letters Colors
+    
+    static let purpleCustom = UIColor(red: 170/255, green: 115/255, blue: 209/255, alpha: 1)
+    static let blueCustom = UIColor(red: 15/255, green: 209/255, blue: 236/255, alpha: 1)
+    static let yellowCustom = UIColor(red: 240/255, green: 194/255, blue: 72/255, alpha: 1)
+    static let pinkBaby = UIColor(red: 245/255, green: 166/255, blue: 195/255, alpha: 1)
+    static let redCustom = UIColor(red: 221/255, green: 0/255, blue: 0/255, alpha: 1)
+    
+    // MARK: - Random UIColor Elements
+    
+    static func random(from colors: [UIColor]) -> UIColor? {
+        return colors.randomElement()
+    }
+
 }
