088e87f56e3ad41d56efc81f9eccc3bcebbc5511
lidianegb <lidianegomesbarbosa@gmail.com>
2021-11-09 09:57:13 -0300

    new extension

--- Letrando/Letrando/Utils/Extensions/CGPoint.swift
+++ Letrando/Letrando/Utils/Extensions/CGPoint.swift
@@ -6,8 +6,20 @@
 //
 
 import Foundation
-import UIKit
+import ARKit
+
 extension CGPoint {
+    
+    /// Extracts the screen space point from a vector returned by SCNView.projectPoint(_:).
+    init(_ vector: SCNVector3) {
+        self.init(x: CGFloat(vector.x), y: CGFloat(vector.y))
+    }
+
+    /// Returns the length of a point when considered as a vector. (Used with gesture recognizers.)
+    var length: CGFloat {
+        return sqrt(x * x + y * y)
+    }
+    
     func distance(to point: CGPoint) -> CGFloat {
         return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2))
     }
