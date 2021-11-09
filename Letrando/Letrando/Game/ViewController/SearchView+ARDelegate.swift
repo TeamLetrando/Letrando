4d3a393316703a32020848d18ef69362cca749ae
lidianegb <lidianegomesbarbosa@gmail.com>
2021-11-09 09:58:32 -0300

    Arkit

--- Letrando/Letrando/Game/ViewController/SearchView+ARDelegate.swift
+++ Letrando/Letrando/Game/ViewController/SearchView+ARDelegate.swift
@@ -13,32 +13,36 @@ import ARKit
 extension SearchViewController : ARSCNViewDelegate, ARSessionDelegate {
     
     func renderer (_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard !isPlaneAdded else { return }
-        isPlaneAdded = true
+        guard !isLettersGenerated else { return }
+        isLettersGenerated = true
         
-        let plane = generatePlane(anchor: anchor)
-        node.addChildNode(plane)
-        
-        let nodes = generateNodes(word: word)
-        generatePositions(planeSize: CGSize(width: plane.width, height: plane.height), nodes: nodes)
-        addNodesToPlane(plane: plane, nodes: nodes)
-        
-        delegate?.changeLettersStackHiding(for: !isPlaneAdded)
-        delegate?.changeMessageLabelHiding(for: isPlaneAdded)
+        let letters = generateNodes(word: word)
+        generatePositions(planeSize: planeSize, nodes: letters)
+        addNodesToScene(nodes: letters)
     }
     
-    func generatePlane(anchor: ARAnchor) -> Plane {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { fatalError("Fail to cast to ARPlaneAnchor") }
-        return Plane(planeAnchor)
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if !isLettersAdded {
+            isLettersAdded = true
+            showLetters(nodes: sceneController.textNode, parent: node)
+        }
+        
+        delegate?.changeLettersStackHiding(for: !isLettersAdded)
+        delegate?.changeMessageLabelHiding(for: isLettersAdded)
     }
-    
-    func getNodeFromFile(name: String, format: String) -> SCNNode? {
+
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        isLettersAdded = false
+    }
+ 
+    private func getNodeFromFile(name: String, format: String) -> SCNNode? {
         guard let urlPath = Bundle.main.url(forResource: name,
                                             withExtension: format) else {
             fatalError("Fail to get url of file \(name).\(format)") }
         
         let scene = try? SCNScene(url: urlPath, options: [.checkConsistency: true])
         let node = scene?.rootNode
+        node?.geometry = SCNGeometry()
         node?.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
         node?.physicsBody?.categoryBitMask = BodyType.letter.rawValue
         node?.scale = SCNVector3(Float(0.02), Float(0.02), Float(0.02))
@@ -47,7 +51,7 @@ extension SearchViewController : ARSCNViewDelegate, ARSessionDelegate {
         return node
     }
     
-    func generateNodes(word: Word?) -> [SCNNode?] {
+    private func generateNodes(word: Word?) -> [SCNNode?] {
         let letters = word?.breakInLetters()
         var nodes: [SCNNode?] = []
         letters?.forEach { letter in
@@ -56,11 +60,16 @@ extension SearchViewController : ARSCNViewDelegate, ARSessionDelegate {
         return nodes
     }
     
-    func addNodesToPlane(plane: Plane, nodes: [SCNNode?]) {
+    private func addNodesToScene(nodes: [SCNNode?]) {
         nodes.forEach { node in
-            plane.addChildNode(node ?? SCNNode())
             sceneController.addLetterToScene(letterNode: node ?? SCNNode())
         }
     }
     
+    private func showLetters(nodes: [SCNNode?], parent: SCNNode) {
+        nodes.forEach { node in
+            node?.removeFromParentNode()
+            parent.addChildNode(node ?? SCNNode())
+        }
+    }
 }
