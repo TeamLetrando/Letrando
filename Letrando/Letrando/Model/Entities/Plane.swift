0c276ca3ce6b78079fbec953a89ea4dcbb8202ba
lidianegb <lidianegomesbarbosa@gmail.com>
2021-11-09 09:58:07 -0300

    update plane

--- Letrando/Letrando/Model/Entities/Plane.swift
+++ Letrando/Letrando/Model/Entities/Plane.swift
@@ -15,8 +15,8 @@ class Plane: SCNNode {
     private var planeNode: SCNNode?
     private var shadowPlaneGeometry: SCNPlane?
     private var shadowNode: SCNNode?
-    lazy var width: CGFloat = planeGeometry?.width ?? .zero
-    lazy var height: CGFloat = planeGeometry?.height ?? .zero
+    var planeSize: CGSize = .zero
+    var planeIdentifier = UUID()
     
     init(_ anchor: ARPlaneAnchor) {
         planeAnchor = anchor
@@ -30,7 +30,9 @@ class Plane: SCNNode {
         addChildNode(planeNode ?? SCNNode())
         addChildNode(shadowNode ?? SCNNode())
         
-        position = SCNVector3(anchor.center.x, .zero, anchor.center.z)
+        position = SCNVector3(anchor.center.x, -0.1, anchor.center.z)
+        planeSize = CGSize(width: planeGeometry?.width ?? .zero, height: planeGeometry?.height ?? .zero)
+        planeIdentifier = planeAnchor.identifier
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -71,5 +73,23 @@ class Plane: SCNNode {
         shadowNode?.transform = plane.transform
         shadowNode?.castsShadow = false
     }
+    
+    func update(_ anchor: ARPlaneAnchor) {
+            planeAnchor = anchor
+
+            planeGeometry?.width = CGFloat(anchor.extent.x)
+            planeGeometry?.height = CGFloat(anchor.extent.z)
+
+            shadowPlaneGeometry?.width = CGFloat(anchor.extent.x)
+            shadowPlaneGeometry?.height = CGFloat(anchor.extent.z)
+
+        position = SCNVector3Make(anchor.center.x, -0.1, anchor.center.z)
+            
+            let planeNode = self.childNodes.first
+            planeNode?.physicsBody = SCNPhysicsBody(type: .static,
+                                                   shape: SCNPhysicsShape(geometry: planeGeometry ?? SCNPlane(),
+                                                                          options: nil))
+            planeNode?.physicsBody?.categoryBitMask = BodyType.plane.rawValue
+        }
     
 }
